9f6feb4e586b133b3cbaced595d09d8a8a05949b
Drew Terry <drew.terry@skookum.com>
2019-02-12 11:02:05 -0500

    more cdns and arranging script locations

--- js/main.js
+++ js/main.js
@@ -18,30 +18,30 @@ $(document).ready(function(){
 		$('header .burger_icon').toggleClass('active');
 	});	
 	
-//wow.js on scroll animations initialization
-	wow = new WOW(
-	    {
-		  animateClass: 'animated',
-		  mobile: false,
-		  offset: 50
-		}
-	);
-	wow.init();
+// //wow.js on scroll animations initialization
+// 	wow = new WOW(
+// 	    {
+// 		  animateClass: 'animated',
+// 		  mobile: false,
+// 		  offset: 50
+// 		}
+// 	);
+// 	wow.init();
 	
 //parallax effect initialization
 	$('.hero').parallax("50%", 0.3);
 	
-//Nice scroll initialization
-	$("html").niceScroll({
-		scrollspeed: 50,
-		autohidemode : false,
-		cursorwidth : 8,
-		cursorborderradius: 8,
-		cursorborder : "0",
-		background : "rgba(48, 48, 48, .4)",
-		cursorcolor : '#1f1f1f',
-		zindex : 999
-	});
+// //Nice scroll initialization
+// 	$("html").niceScroll({
+// 		scrollspeed: 50,
+// 		autohidemode : false,
+// 		cursorwidth : 8,
+// 		cursorborderradius: 8,
+// 		cursorborder : "0",
+// 		background : "rgba(48, 48, 48, .4)",
+// 		cursorcolor : '#1f1f1f',
+// 		zindex : 999
+// 	});
 	
 //Testimonials slider initialization
 	$("#tslider").owlCarousel({
@@ -55,6 +55,7 @@ $(document).ready(function(){
 		autoPlay : true,
 		transitionStyle : "fade"
 	});	
+	
 //Mailchimp subscription form initialization
 	$('#submit_form').submit(function(){
 		$('#mc_submit').attr('disabled', 'disabled');		
